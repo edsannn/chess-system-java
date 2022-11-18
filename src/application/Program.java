b84b0dc9d55ebcd013f38cb73ccfe8e89ee25e48
Edson Santos <cmo.edson@gmail.com>
2022-11-18 13:39:50 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,12 @@ public class Program {
 					
 				}
 				
+				if(chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");;
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
